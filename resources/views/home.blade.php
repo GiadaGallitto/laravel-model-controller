897699ab1bece773286ecb303b9e2b87c4a497aa
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-09 16:44:27 +0100

    Fix cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
         <div class="row d-flex justify-content-center">
             @forelse ($movies as $movie)
             <div class="col-2 m-1 text-center text-dark p-4 border bg-light bg-gradient bg-opacity-75 border-dark rounded-3">
-                <h4>{{$movie->title}}</h4>
-                <h5>{{$movie->original_title}}</h5>
-                <p>{{$movie->nationality}}</p>
-                <span>{{$movie->date}}</span>
-                <h6>{{$movie->vote}}</h6>
+                <h4 class="mb-4">{{$movie->title}}</h4>
+                <h6 class="mb-3">{{$movie->original_title}}</h6>
+                <p><span><strong>Lang: </strong></span>{{$movie->nationality}}</p>
+                <p><span><strong>Date: </strong></span>{{$movie->date}}</p>
+                <h6><span><strong>Vote: </strong></span>{{$movie->vote}}</h6>
             </div>
             @empty
                 <p>Nessun film presente</p>

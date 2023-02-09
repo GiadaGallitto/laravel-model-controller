e480bcb26a688c4356ea88fc34a9cfd9fe09eaad
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-09 16:41:02 +0100

    Add card with movies infos from database, add css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('main-content')
+    <h1 class="text-center mb-5">Movies List</h1>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            @forelse ($movies as $movie)
+            <div class="col-2 m-1 text-center text-dark p-4 border bg-light bg-gradient bg-opacity-75 border-dark rounded-3">
+                <h4>{{$movie->title}}</h4>
+                <h5>{{$movie->original_title}}</h5>
+                <p>{{$movie->nationality}}</p>
+                <span>{{$movie->date}}</span>
+                <h6>{{$movie->vote}}</h6>
+            </div>
+            @empty
+                <p>Nessun film presente</p>
+            @endforelse
+        </div>
+    </div>
 
 @endsection
